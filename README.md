cec5371912f94057da747cc93232e2d01f3149ee
ei1ro_abe <ei1ro.abe@gmail.com>
2018-10-26 16:47:17 +0900

    課題_step2_1 UI調整

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 #step2_1
 ##javascriptのみで九九表を作成
+###実行方法　index.htmlをブラウザで開く
--- task_js/step2_1/js/main.js
+++ task_js/step2_1/js/main.js
@@ -1,9 +1,22 @@
-document.write('<table border="1">');
-for (var i = 1; i < 10; i++) {
-	document.write('<tr>');
-	for (var j = 1; j < 10; j++) {
-		document.write('<td>'+ i*j + '</td>');
+window.onload = function (){
+	var table = document.createElement('table');
+	table.style.borderCollapse = 'collapse';
+
+	for (var i = 1; i < 10; i++) {
+		var tr = document.createElement('tr');
+		for (var j = 1; j < 10; j++) {
+			var td = document.createElement('td');
+			var t_style = td.style;
+			t_style.width = '30px';
+			t_style.height = '30xp';
+			t_style.textAlign = 'center';
+			t_style.border = '1px solid';
+
+			td.innerHTML = i * j;
+			tr.appendChild(td);
+		}
+		table.appendChild(tr);
 	}
-	document.write('</tr>');
+	document.body.appendChild(table);
 }
-document.write('</table>');
+
