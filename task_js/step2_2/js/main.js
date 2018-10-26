53fad0ef246e114e8b72a6b3c6d48349edda3fef
ei1ro_abe <ei1ro.abe@gmail.com>
2018-10-26 20:50:15 +0900

    課題_step2_2 修正、jqueryの格納先変更

--- task_js/step2_2/js/main.js
+++ task_js/step2_2/js/main.js
@@ -1,14 +1,14 @@
 $(function() {
-	var table = $('<table border="1">').appendTo('body');
+	$('body').append('<table id="table" border="1"></table>');
 
-	var row, col;
-	for (row = 1; row < 10; row++) {
-		var tr = $('<tr>').appendTo(table);
-		for (col = 1; col < 10; col++) {
-			var td = $('<td>').appendTo(tr);
-			td.text(row * col).appendTo(tr);
-			if ((td % 2) != 0) {
-				$('td:odd').css({'background-color': '#555', 'color': '#fff'});
+	var table = $("#table");
+	for (var i = 1; i < 10; i++) {
+		table.append("<tr class='row_" + i + "'></tr>");
+		for (var j = 1; j < 10; j++) {
+			$(".row_" + i).append("<td>" + (i * j) + "</td>");
+			$('td').css({'width': '30px', 'height': '30px', 'text-align': 'center'});
+			if (($('td') % 2) != 0) {
+				$('td:odd').css({'background-color': '#555', 'color': '#fff',});
 			}
 		}
 	}
